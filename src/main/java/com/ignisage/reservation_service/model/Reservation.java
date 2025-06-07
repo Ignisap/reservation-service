e3aa095081610ba21ff8280b172b26f3597053c9
Night-human <agexandro@gmail.com>
2025-06-07 02:05:49 -0600

    Reservation model added

--- src/main/java/com/ignisage/reservation_service/model/Reservation.java
+++ src/main/java/com/ignisage/reservation_service/model/Reservation.java
@@ -1,9 +1,28 @@
 package com.ignisage.reservation_service.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Entity
 @Table(name = "reservation")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private UUID userId;
+
+    @Column(nullable = false)
+    private LocalDate reservationDate;
+
+    @Column(nullable = false)
+    private String status;
 }
